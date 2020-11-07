68ba485ab501ae3bdc8ca15320f0f56e253b1d58
Alison Williams <apw5011@gmail.com>
2020-11-07 16:34:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(array)
   if array.length == 0
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     array.each_with_index do |name, index|
       place = index + 1
