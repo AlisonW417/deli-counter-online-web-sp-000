b9c09dd40a076aa08991e07a961eb83170cef068
Alison Williams <apw5011@gmail.com>
2020-11-07 16:00:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,15 @@
 # Write your code here.
-def line
-  
+def line(array)
+  if array.length >= 1
+    array.each do |name|
+      place = array[index] + 1 
+      puts "The links is currently: #{place}. #{name}"
+    end
+  else
+    return "The line is currently empty"  
+  end
 end
 
-def take_a_number(katz_deli, name)
+def take_a_number(array, name)
   
 end
